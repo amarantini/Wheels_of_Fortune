1e222db6a988bd1e616135270a12c07b5fd17cd3
Qiru Hu <qiru@cs.ucsb.edu>
2021-01-16 12:04:21 -0500

    qh - Add more details to Game and Player

--- javascript/src/main/components/game.jsx
+++ javascript/src/main/components/game.jsx
@@ -1,4 +1,4 @@
-import {Button} from "react-bootstrap";
+import {Row, Col, Form, Button } from "react-bootstrap";
 
 
 //Square: contain alphabet
@@ -23,18 +23,6 @@ class Board extends React.Component {
             //xIsNext: true,
         };
     }
-  
-    /*handleClick(i) {
-        const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
-    }*/
     
     //TODO:
     renderSquare(i) {
@@ -79,57 +67,118 @@ class Board extends React.Component {
     }
 }
 
+
 class Player extends Reaxt.Component {
     constructor(props) {
         super(props);
+        this.puzzle = puzzle;
+        this.playerIndex = playerIndex;
+        this.puzzleDict = {};
+        for(let i=0; i<puzzle.length; i++){
+            if(puzzle[i] != " "){
+                if(!(puzzle[i] in puzzleDict)){
+                    puzzleDict[puzzle[i]] = 1;
+                } else {
+                    puzzleDict[puzzle[i]] += 1;
+                }
+            }
+        }
         this.state = {
             award: 0,
             playing: true,
-            reward: 0
+            reward: 0,
+            guess: ""
         };
     }
 
-    //TODO: The wheel has 24 spaces ($500*5,$600*5,$700*5,$900*5, $2500*1, Bankrupt*2, LoseATurn*1)
+    //TODO: The wheel has 24 spaces ($500*4,$600*4,$700*4,$800*4,$900*4, $2500*1, Bankrupt*2, LoseATurn*1)
     spinTheWheel() {
-        return {
-            //reward = ?
-        }
+        const rewards = [500,600,700,800,900,2500,"Bankrupt",
+                        500,600,700,800,900,"Bankrupt",
+                        500,600,700,800,900,"Lose a turn",
+                        500,600,700,800,900];
+        return rewards[Math.floor(Math.random() * 24)];
     }
 
     //TODO: spinTheWhell
     //if bankrupt, set award to 0, else if lose a turn, next player's turn;
     //if alphabet in phrase, add num of alphabet*reward to award and spinTheWhellagain, 
     //else lose the turn to next player
-    guess(alphabet) {
-        /*
-        reward = spinTheWheel
-        if(reward===Bankrupt) {
-            award=0
-            next player
-        } else if (reward===LoseATurn) {
-            next player
+    handleGuess() {
+        const alphabet = this.guess;
+        const reward = spinTheWheel();
+        if(reward==="Bankrupt") {
+            this.setState({
+                award:0,
+                playing: false
+            });
+        } else if (reward==="Lose a turn") {
+            this.setState({
+                playing: false
+            });
         }
-        if(alphabet already called) next Player
-        if(alphabet in phrase){
-            award += reward*num of alphabet
-            next guess
+        //if(alphabet already called) next Player
+        if(alphabet in puzzleDict){
+            let newAward = award + puzzleDict[alphabet]*reward;
+            this.setState({
+                award: newAward
+            });
+            this.guess();
         } else {
-            next player
+            this.setState({
+                playing: false
+            });
         }
-        */
+
     }
 
     //TODO: each vowel cost $250
     //Contestants may continue to buy vowels so long as they have enough money to keep doing so, 
     //until all of the vowels in the puzzle have been revealed
-    buyVowel(vowel) {
-
+    handleBuyVowel() {
+        const vowel = this.guess;
+        //if not all the vowels are reviewed
+        this.setState({
+            award: this.award-250
+        });
+        //if(vowel in puzzleDict)
     }
 
     //TODO: if true, declare winner
     //else lose a turn
-    solveThePuzzle(phrase) {
+    handleSolveThePuzzle() {
 
+    }
+
+    handleChange(event) {
+        this.setState({guess: event.target.value});
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleGuess}>
+                <label>
+                Guess:
+                <input name="guess" type="text" value={this.state.guess} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Guess" />
+                <br />
+            <form onSubmit={this.handleBuyVowel}>
+                <label>
+                Buy a vowel:
+                <input name="buyVowel" type="text" onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Buy" />
+                <br />
+            </form>
+            <form onSubmit={this.handleSolveThePuzzle}></form>
+                <label>
+                Solve the puzzle:
+                <input name="solvePuzzle" type="text" onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
     }
 }
 
@@ -137,23 +186,39 @@ class Player extends Reaxt.Component {
 class Game extends React.Component {
     constructor(props) {
         super(props);
+        const data = require('../../../resource/puzzles.json');
+        const keys = Object.keys(data);
+        const dataSize=keys.length;
+        this.puzzle=keys[Math.floor(Math.random()*dataSize)];
+        this.hint=data[puzzle];
+        this.puzzleDict = {};
+        for(let i=0; i<puzzle.length; i++){
+            if(puzzle[i] != " "){
+                if(!(puzzle[i] in puzzleDict)){
+                    puzzleDict[puzzle[i]] = 1;
+                } else {
+                    puzzleDict[puzzle[i]] += 1;
+                }
+            }
+        }
         this.state = {
-            //squares: Array(9).fill(null),
-            player1: Player(),
-            player2: Player()
+            currPlayerIndex: "1"
         };
     }
+
     //TODO:
     render() {
         return (
             <div className="game">
-            <div className="game-board">
-                <Board />
-            </div>
-            <div className="game-info">
-                <div>{/* status */}</div>
-                <ol>{/* TODO */}</ol>
-            </div>
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-player">
+                    <Row>
+                        <Col><Player puzzle={phrase} playerIndex="1"/></Col>
+                        <Col><Player puzzle={phrase} layerIndex="2"/></Col>
+                    </Row>
+                </div>
             </div>
         );
     }
